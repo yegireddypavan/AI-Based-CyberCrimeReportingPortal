028db6ce04df315ac5f040e9fcb420aa20db8b79
yegireddypavan <pavan_yegireddy@srmap.edu.in>
2025-10-06 15:30:07 +0530

    Initial commit with frontend + backend + AI service

---
+++ backend/demo/src/test/java/com/example/CyberCrime/CybercrimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CyberCrime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybercrimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
