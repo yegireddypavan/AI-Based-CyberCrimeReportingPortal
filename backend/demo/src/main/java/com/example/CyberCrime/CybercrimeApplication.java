ed8e110069800cde3fd2b2bfd6bbc16174148502
yegireddypavan <pavan_yegireddy@srmap.edu.in>
2025-10-14 20:02:12 +0530

    done

--- backend/demo/src/main/java/com/example/CyberCrime/CybercrimeApplication.java
+++ backend/demo/src/main/java/com/example/CyberCrime/CybercrimeApplication.java
@@ -1,5 +1,6 @@
 package com.example.CyberCrime;
 
+import org.springframework.aop.scope.ScopedProxyUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,5 @@ public class CybercrimeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CybercrimeApplication.class, args);
 	}
-
+	System.out.println("hello world");
 }
